96e7a44f1a254e0a0781d4efe82bc66c47e032cc
Andrés Cortés <126137543+andrescortes09@users.noreply.github.com>
2023-04-01 18:42:31 +0000

    Ejercicio 2 POO

--- main.cpp
+++ main.cpp
@@ -1,27 +1,85 @@
-// C++ program to demonstrate the use of rand() to get value
-// in a range of 0 to N
-#include <ctime>
-#include <cstdlib>
+/*Crear 3 clases (Persona, Profesor, estudiante), que se pueden presentar. El estudiante que curse 9no o superior, puede inscribirse en el curso de programación. En el programa principal, se presentan los 3 objetos, y si el estudiante puede o no hacer el curso de programación.
+
+Los datos miembros necesarios de la clase, son definidos por el programador
+
+La cantidad de métodos de cada clase, son definidos por el programador*/
+
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main()
-{
-	float Vector[9];
-	int O = 0;
-  srand(time(NULL));
-	// This program will create some sequence of random
-	// numbers on every program run within range N
-	for (int i = 0; i < 10; i++){
-		Vector[i] = rand() % 26;
-		cout << Vector[i] << " - ";
+// Clase Persona
+class Persona {
+private:
+    string nombre;
+    int edad;
+
+public:
+    Persona(string nombre, int edad) {
+        this->nombre = nombre;
+        this->edad = edad;
     }
-    for (int i=0; i <= 10; i = i+1){
-        cout << "\nSeleccione La Posición Deseada: \n";
-        cin >> O;
-        cout << Vector + (O - 1) << endl;
-        cout << Vector[O - 1] << endl;
+
+    string getNombre() {
+        return nombre;
     }
-	return 0;
-	
+
+    int getEdad() {
+        return edad;
+    }
+};
+
+// Clase Estudiante, que hereda de Persona
+class Estudiante : public Persona {
+private:
+    int grado;
+
+public:
+    Estudiante(string nombre, int edad, int grado) : Persona(nombre, edad) {
+        this->grado = grado;
+    }
+
+    int getGrado() {
+        return grado;
+    }
+
+    bool puedeInscribirse() {
+        return grado >= 9;
+    }
+};
+
+// Clase Profesor, que hereda de Persona
+class Profesor : public Persona {
+private:
+    string materia;
+
+public:
+    Profesor(string nombre, int edad, string materia) : Persona(nombre, edad) {
+        this->materia = materia;
+    }
+
+    string getMateria() {
+        return materia;
+    }
+};
+
+int main() {
+    // Crear objetos
+    Persona persona("Juan", 35);
+    Estudiante estudiante("Maria", 15, 9);
+    Profesor profesor("Pedro", 50, "Programacion");
+
+    // Mostrar información
+    cout << "Nombre de la persona: " << persona.getNombre() << endl;
+    cout << "Edad de la persona: " << persona.getEdad() << endl;
+    cout << "Nombre del estudiante: " << estudiante.getNombre() << endl;
+    cout << "Edad del estudiante: " << estudiante.getEdad() << endl;
+    cout << "Grado del estudiante: " << estudiante.getGrado() << endl;
+    cout << "¿Puede inscribirse en programacion? " << (estudiante.puedeInscribirse() ? "Si" : "No") << endl;
+    cout << "Nombre del profesor: " << profesor.getNombre() << endl;
+    cout << "Edad del profesor: " << profesor.getEdad() << endl;
+    cout << "Materia del profesor: " << profesor.getMateria() << endl;
+
+    return 0;
 }
